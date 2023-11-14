d2756b562096da72e61f000f383a22751bf5c8b7
Kilian Boute <kilian.boute@gmail.com>
2023-11-14 17:13:28 +0100

    Fix correctly updating and displaying weather for current location

--- scripts.js
+++ scripts.js
@@ -46,33 +46,34 @@ const displayLocations = (location) => {
     locationItem.longitude = location.longitude;
     locationItem.latitude = location.latitude;
     locationItem.timeZone = location.timezone;
+    locationItem.name = location.name;
+    locationItem.area = location.admin1;
+    locationItem.country = location.country;
     locationItem.addEventListener('click', () => {
         locationInfo.innerHTML = "";
         cardContainer.innerHTML = "";
         locationDisplay.style.display = "none";
-        console.log(locationItem.longitude + " " + locationItem.latitude + " " + locationItem.timeZone);
-        getWeatherForLocation(locationItem.longitude, locationItem.latitude, locationItem.timeZone);
+        console.log("test");
+        getWeatherForLocation(locationItem.longitude, locationItem.latitude, locationItem.name, locationItem.area, locationItem.country);
     })
     locationDisplay.appendChild(locationItem);
 }
 
-const getWeatherForLocation = (longitude, latitude) => {
+const getWeatherForLocation = (longitude, latitude, name, area, country) => {
     fetch("https://api.open-meteo.com/v1/forecast?latitude=" + latitude + "&longitude="
         + longitude + "&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=auto")
         .then(response => response.json())
         .then(data => {
-            console.log(data.daily);
-            displayWeatherForLocation(data.daily);
+            displayWeatherForLocation(data.daily, name, area, country);
         });
 }
 
-const displayWeatherForLocation = (locationData) => {
-    console.log(locationData)
+const displayWeatherForLocation = (locationData, name, area, country) => {
 
     const locationHeader = document.createElement('h1');
-        locationHeader.textContent = "GHENT";
+        locationHeader.textContent = name;
     const locationArea = document.createElement('h3');
-        locationArea.textContent = "testext"
+        locationArea.textContent = area + ", " + country;
 
     locationInfo.appendChild(locationHeader);
     locationInfo.appendChild(locationArea);
@@ -82,7 +83,8 @@ const displayWeatherForLocation = (locationData) => {
             cardElement.className = "card";
         const cardDay = document.createElement('h3');
         let timeDate = new Date(locationData.time[i]);
-            cardDay.textContent = weekdays[timeDate.getDay()];
+            if(i === 0){cardDay.textContent = "Today";}
+            else {cardDay.textContent = weekdays[timeDate.getDay()];}
             
         const cardImage = document.createElement('img');
         const imgSrc = weatherCodes[locationData.weather_code[i]].day.image;
@@ -106,7 +108,9 @@ async function getWeatherOnSubmit(location) {
     const response = await fetch("https://geocoding-api.open-meteo.com/v1/search?name=" + location + "&count=10&language=en&format=json")
     const data = await response.json();
     locationDisplay.style.display = "none";
-    getWeatherForLocation(data.results[0].longitude, data.results[0].latitude);
+    locationInfo.innerHTML = "";
+    cardContainer.innerHTML = "";
+    getWeatherForLocation(data.results[0].longitude, data.results[0].latitude, data.results[0].name, data.results[0].admin1, data.results[0].country);
 }
 
 searchBarForm.addEventListener('submit', (e) => {
